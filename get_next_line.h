da6433319f608a46548e3c64d1f576295666f97a
nils <nilslucas@wanadoo.fr>
2023-12-12 18:10:36 +0100

    v.1

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,25 @@
 /*   By: niabraha <niabraha@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:54:18 by niabraha          #+#    #+#             */
-/*   Updated: 2023/12/07 16:54:19 by niabraha         ###   ########.fr       */
+/*   Updated: 2023/12/12 16:03:14 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *src);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+
+#endif
--- main.c
+++ main.c
@@ -1,16 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: niabraha <niabraha@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/07 16:54:15 by niabraha          #+#    #+#             */
-/*   Updated: 2023/12/12 16:04:01 by niabraha         ###   ########.fr       */
+/*   Created: 2023/12/12 14:56:02 by niabraha          #+#    #+#             */
+/*   Updated: 2023/12/12 15:10:33 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int fd)
-{
+#include "get_next_line.h"
 
+int main ()
+{
+	int fd;
+	char buffer[256];
+	
+	fd = open("./textou.txt", 1);
 }
+
+
+/* 
+read(int fd, void* buf (stash), size_t nbytes (Nombre de char qu'elle lit a chaque fois))
+
+
+ */
