d53878d5e2b4e2d0901c03c806fdc554cc832af6
nils <nilslucas@wanadoo.fr>
2023-12-14 12:42:58 +0100

    v.0.3

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 11:13:32 by niabraha          #+#    #+#             */
-/*   Updated: 2023/12/14 12:23:38 by niabraha         ###   ########.fr       */
+/*   Updated: 2023/12/14 12:26:34 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,16 @@ read(int fd, void* buf (stash), size_t nbytes (Nombre de char qu'elle lit a chaq
  */
 int main()
 {
-	
+	char* fileName = "textou.txt";
+
+	int fd = open(fileName, O_RDWR);
+
+	char buffer[BUFFER_SIZE];
+
+	int bytesRead = read(fd, buffer, sizeof(buffer));
+
+	printf("%d bytes read!\n", bytesRead);
+	printf("File Contents: %s\n", buffer);
+
+	return 0;
 }
