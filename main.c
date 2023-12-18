044003e9dc20fa91f97f782a8ee1e816c71723bf
Nils Abraham <44902217+NilsLeVrai@users.noreply.github.com>
2023-12-18 14:38:17 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,31 +3,72 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niabraha <niabraha@42.fr>                  +#+  +:+       +#+        */
+/*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 11:13:32 by niabraha          #+#    #+#             */
-/*   Updated: 2023/12/14 12:26:34 by niabraha         ###   ########.fr       */
+/*   Updated: 2023/12/18 14:24:42 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
-/* 
-read(int fd, void* buf (stash), size_t nbytes (Nombre de char qu'elle lit a chaque fois))
+#include <stdio.h>
 
- */
+size_t	ft_strlen(const char *s)
+{
+	int		i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned int		i;
+	unsigned char		char_find;
+	size_t				len;
+
+	char_find = (unsigned char) c;
+	i = 0;
+	len = (size_t) ft_strlen(s);
+	while (i <= (unsigned int) len)
+	{
+		if (s[i] == char_find)
+			return ((char *) &s[i]);
+		i++;
+	}
+	return (NULL);
+}
+
+
+char	*get_next_line(int fd)
+{
+	int			bytes_read;
+	char		*stash;
+
+	stash = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!stash)	
+		return (NULL);
+	bytes_read = read(fd, stash, BUFFER_SIZE);
+	printf()
+	if (fd < 0 || BUFFER_SIZE < 0)
+		return (NULL);
+	return (stash);
+}
+
 int main()
 {
-	char* fileName = "textou.txt";
+	int		fd;
+	char	*line;
+	int		len;
 
-	int fd = open(fileName, O_RDWR);
-
-	char buffer[BUFFER_SIZE];
-
-	int bytesRead = read(fd, buffer, sizeof(buffer));
-
-	printf("%d bytes read!\n", bytesRead);
-	printf("File Contents: %s\n", buffer);
-
-	return 0;
+	len = 0;
+	fd = open("textou.txt", O_RDONLY);
+	line = get_next_line(fd);
+	len++;
+	printf("[%d]:%s\n", len, line);
+	line = NULL;
+	close(fd);
+	return (0);
 }
