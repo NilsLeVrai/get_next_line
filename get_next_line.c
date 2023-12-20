b201b0f6165ee718865bb1a55b383e75da97295b
NilsLeVrai <nilslucas@wanadoo.fr>
2023-12-20 16:32:31 +0100

    v0.4

--- get_next_line.c
+++ get_next_line.c
@@ -12,10 +12,83 @@
 
 #include "get_next_line.h"
 
+char	*next_line(char *stash)
+{
+	int		i;
+	int		j;
+	char	*new_stash;
+
+	i = 0;
+	j = 0;
+	while (stash[i] != '\n' && stash[i] != '\0')
+		i++;
+	if (stash[i] == '\0')
+	{
+		free(stash);
+		return (NULL);
+	}
+	new_stash = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
+	if (!new_stash)
+		return (NULL);
+	i++;
+	while (stash[i] != '\0')
+		new_stash[j++] = stash[i++];
+	new_stash[j] = '\0';
+	free(stash);
+	return (new_stash);
+}
+
+char	*current_line(char *stash)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	while (stash[i] != '\n' && stash[i] != '\0')
+		i++;
+	line = malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stash[i] != '\n' && stash[i] != '\0')
+	{
+		line[i] = stash[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_parsing(int fd, char *line)
+{
+	int			read_bytes;
+	char		*buffer;
+	static char	*stash;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	read_bytes = 1;
+	while (!ft_strchr(line, '\n') && read_bytes != 0)
+	{
+		read_bytes = read(fd, buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[read_bytes] = '\0';
+		stash = ft_strjoin(line, buffer);
+	}
+	free(buffer);
+	return (stash);
+}
+
 char	*get_next_line(int fd)
 {
-	char		*line;
-	static char	*stash;
+	char	*line;
+	char	*stash;
+	line = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
 		return (NULL);
 	stash = ft_parsing(fd, line);
