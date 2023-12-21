9b534cbb5be7cb8f84b630a98f651269fc5f95a2
NilsLeVrai <nilslucas@wanadoo.fr>
2023-12-21 15:57:39 +0100

    v0.4

--- get_next_line.c
+++ get_next_line.c
@@ -6,93 +6,51 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 16:54:15 by niabraha          #+#    #+#             */
-/*   Updated: 2023/12/20 14:21:00 by niabraha         ###   ########.fr       */
+/*   Updated: 2023/12/21 15:37:25 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*next_line(char *stash)
+char	*ft_clean(char *str)
 {
-	int		i;
-	int		j;
-	char	*new_stash;
+	char	*p;
 
-	i = 0;
-	j = 0;
-	while (stash[i] != '\n' && stash[i] != '\0')
-		i++;
-	if (stash[i] == '\0')
+	p = NULL;
+	if (str)
 	{
-		free(stash);
-		return (NULL);
+		p = ft_strdup(str);
+		free(str);
 	}
-	new_stash = malloc(sizeof(char) * (ft_strlen(stash) - i + 1));
-	if (!new_stash)
-		return (NULL);
-	i++;
-	while (stash[i] != '\0')
-		new_stash[j++] = stash[i++];
-	new_stash[j] = '\0';
-	free(stash);
-	return (new_stash);
+	return (p);
 }
 
-char	*current_line(char *stash)
+char	*ft_clean_stash(char *str)
 {
-	int		i;
-	char	*line;
+}
 
-	i = 0;
-	while (stash[i] != '\n' && stash[i] != '\0')
-		i++;
-	line = malloc(sizeof(char) * (i + 1));
-	if (!line)
-		return (NULL);
-	i = 0;
-	while (stash[i] != '\n' && stash[i] != '\0')
-	{
-		line[i] = stash[i];
-		i++;
-	}
-	line[i] = '\0';
+char	*get_line(char*str)
+{
+	char	*del; // del is the address of the first \n in str
+	char*	line; // line is the line to be returned
+	int		len; // len is the length of the line to be returned
+
+	del = ft_strchr(str, "\n");
+	len = (del - str) + 1;
+	line = ft_substr(str, 0, len);
 	return (line);
-}
-
-char	*ft_parsing(int fd, char *line)
-{
-	int			read_bytes;
-	char		*buffer;
-	static char	*stash;
-
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return (NULL);
-	read_bytes = 1;
-	while (!ft_strchr(line, '\n') && read_bytes != 0)
-	{
-		read_bytes = read(fd, buffer, BUFFER_SIZE);
-		if (read_bytes == -1)
-		{
-			free(buffer);
-			return (NULL);
-		}
-		buffer[read_bytes] = '\0';
-		stash = ft_strjoin(line, buffer);
-	}
-	free(buffer);
-	return (stash);
 }
 
 char	*get_next_line(int fd)
 {
-	char	*line;
-	char	*stash;
-	line = NULL;
+	char		*line;
+	static char	*stash[4096];
+
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
 		return (NULL);
-	stash = ft_parsing(fd, line);
-	line = current_line(stash);
-	stash = next_line(stash);
+	line = get_line(stash[fd]);
+	if (!line)
+		return (&ft_clean(stash[fd]));
+	stash[fd] = ft_clean_stash(stash[fd]);
 	return (line);
 }
